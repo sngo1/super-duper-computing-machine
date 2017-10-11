278d4f377fac8ccc827443a0dea7c25b04d249e9
Valery Chen <cpan3@stuy.edu>
2017-10-11 19:09:32 -0400

    used flash for msgs

--- app.py
+++ app.py
@@ -5,10 +5,13 @@
 # 2017-10-06
 
 # Import all necessities
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import session
+from flask import redirect, url_for
+from flask import flash
 import os
-
-from util import loginMethods
 
 # Create instance of class
 app = Flask(__name__) 
@@ -27,8 +30,7 @@ def loginPage():
     if "username" in session:
         return redirect(url_for("welcome"))     
     else:
-        print "loginMethods.login('')"
-        return loginMethods.login("")
+        return render_template("login.html")
 
 # Authentification Process
 @app.route("/auth", methods=["POST"])
@@ -46,11 +48,15 @@ def authentification():
         # ...render the welcome page by redirecting to login page
         # Login page will recognize that a session is open, so it'll
         # render the welcome page.
+        print "Matches data in database"
         print "URL FOR:", url_for("welcome")
+        flash( "Success!" )
         return redirect(url_for("welcome"))
     # Otherwise, render the login page(again) with an error message.
     else:
-        return loginMethods.login("Incorrect username or password")
+        print "Does not match"
+        flash( "Incorrect Username or Password Given!" )
+        return render_template("login.html")
         
 # Logging out renders the login page.
 # Should it reroute to the login page instead?
@@ -58,14 +64,15 @@ def authentification():
 def logout():
     # End the session
     session.pop("username")
+    print "User has logged out"
     # Render Login Page
-    return loginMethods.login("Logged out!")
+    flash( "You have logged out!" )
+    return redirect(url_for("loginPage"))
 
 @app.route("/welcome")
 def welcome():
     if "username" in session:
-        print "run loginMethods.loggedIn(session['username'])"
-        return loginMethods.loggedIn(session["username"])
+        return render_template("welcome.html")
     else:
         return redirect(url_for("loginPage"))
     
